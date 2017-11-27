6d4d1822044219ccda6ba370032ecdb1deea48c6
Scott Smitelli <scott@smitelli.com>
2017-11-26 21:51:00 -0500

    npm might be a piece of crap

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -e
 SELF_DIR="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )"
 
 cd "$SELF_DIR"
-npm install --prod
+# --no-save stops package-lock.json from being overwritten
+npm install --prod --no-save
 ./node_modules/.bin/r.js -o ./src/js/build.js
 cp -f ./build/css/style.css ./build/js/main.js ./public/
 rm -rf ./build ./node_modules
